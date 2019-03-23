e8d8e70060eeea2ef705edcb77bf25866ae53757
Red Hat Developers Launcher <45641108+redhat-developers-launcher@users.noreply.github.com>
2019-03-23 11:45:11 +0000

    Initial commit

---
+++ .openshiftio/test.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -e
+
+if [ ! -d ".openshiftio" ]; then
+  warning "The script expects the .openshiftio directory to exist"
+  exit 1
+fi
+
+source .openshiftio/openshift.sh
+
+if [ -z "$1" ]; then
+  ORG="openshiftio-vertx-boosters"
+else
+  ORG=$1
+fi
+
+REPO="https://github.com/$ORG/vertx-configmap-booster-redhat"
+echo -e "\n${YELLOW}Using source repository: $REPO ...\n${NC}"
+
+# cleanup
+oc delete build --all
+oc delete bc --all
+oc delete dc --all
+oc delete deploy --all
+oc delete is --all
+oc delete istag --all
+oc delete isimage --all
+oc delete job --all
+oc delete po --all
+oc delete rc --all
+oc delete rs --all
+oc delete statefulsets --all
+oc delete secrets --all
+oc delete configmap --all
+oc delete services --all
+oc delete routes --all
+oc delete template --all
+
+# Deploy the templates and required resources
+oc apply -f .openshiftio/resource.roles.yaml
+oc apply -f .openshiftio/resource.configmap.yaml
+oc apply -f .openshiftio/application.yaml
+
+# Create the application
+oc new-app --template=vertx-configmap-booster -p SOURCE_REPOSITORY_URL="$REPO"
+
+# wait for pod to be ready
+waitForPodState "configmap-vertx" "Running"
+waitForPodReadiness "configmap-vertx" 1
+
+mvn verify -Popenshift-it -Denv.init.enabled=false
